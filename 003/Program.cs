474d972be51d3b5bf58232b04439744a116c8640
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-06 02:32:30 +0900

    Create Program.cs

---
+++ 003/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _003
+{
+    class Program
+    {
+        //与えられた数までの素数を列挙する
+        static void Main(string[] args)
+        {
+           var n = long.Parse(Console.ReadLine());
+
+            foreach(var i in GetPrimeNumber(n)){
+                Console.WriteLine(i);
+            }
+
+        }
+
+        static IEnumerable<long> GetPrimeNumber(long n){
+            if(n >= 2){
+                yield return 2;
+            }
+
+            for(var i = 3; i <= n; i += 2){
+                var flag = true;
+                var sqrtnum = Math.Sqrt(i);
+                for(var j = 3; j <= sqrtnum; j += 2){
+                    if(i % j == 0){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    yield return i;
+                }
+            }
+        }
+    }
+}
