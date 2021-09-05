f2c0d78a1cd6376b527255482d896f47710e5e4a
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-06 02:32:26 +0900

    Create Program.cs

---
+++ 001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = long.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(string.Format("Hello World! {0}",i + 1));                
+            }
+        }
+    }
+}
