33f9d2d116438d581b0cc4aa7204d3f9f94222a7
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-06 04:01:36 +0900

    Create Program.cs

---
+++ 009/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new float[100];
+            var x = 12345;
+            for(var i = 0; i < 100; i++){
+                x = (997 * x + 1) % 65536;
+                arr[i] = (float)x / 65536;
+            }
+
+            Console.WriteLine(string.Join(" ",arr.Select(n => n.ToString())));
+            Console.WriteLine("平均:{0}",arr.Sum() / 100);
+        }
+    }
+}
