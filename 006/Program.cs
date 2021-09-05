4104cb48a22c9b7a59a788dfb086712d6e4474f9
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-06 04:01:25 +0900

    Create Program.cs

---
+++ 006/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var year = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}",IsLeapYear(year) ? "閏年" : "平年");
+        }
+
+        static bool IsLeapYear(int year){
+            var ret = false;
+            if(year % 4 == 0){
+                ret = (year % 100 == 0 && year % 400 != 0) ? false : true;
+            }else{
+                ret = false;
+            }
+            return ret;
+        }
+    }
+}
