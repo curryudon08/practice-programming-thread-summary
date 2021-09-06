956f7d5c2b6611b0397ae6f722ef7e3083fa0a2d
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-07 00:01:20 +0900

    Create Program.cs

---
+++ 013/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new int[]{3, 5, 2, 4, 2};
+
+            for(var i = 0; i < a.Length; i++){
+                if(i != 0){
+                    a[i] = 0;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", a.Select(x => x.ToString())));
+        }
+    }
+}
