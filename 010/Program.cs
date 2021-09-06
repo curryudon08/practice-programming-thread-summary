e5d6c852c27f52c93066a65b799ec53c7764394f
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-06 23:11:50 +0900

    Create Program.cs

---
+++ 010/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace _010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var x = r.Next(1, 101);
+
+            while(true){
+                var n = int.Parse(Console.ReadLine());
+                if(n == x){
+                    Console.WriteLine("正解");
+                    break;
+                }else if(n > x){
+                    Console.WriteLine("もっと小さい");
+                }else{
+                    Console.WriteLine("もっと大きい");
+                }
+            }
+        }
+    }
+}
