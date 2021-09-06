860a05921daead89f63fc95754b7fac13b38f762
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-07 00:23:11 +0900

    Create Program.cs

---
+++ 019/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 200; i++){
+                if(IsAho(i)){
+                    Console.Write("aho ",i);
+                }else{
+                    Console.Write("{0} ",i);
+                }                
+            }
+        }
+
+        static bool IsAho(int n)
+        {
+            var aho = false;
+            if(n % 3 == 0){
+                aho = true;
+            }else{
+                while(n > 0){
+                    if(n % 10 == 3){
+                        aho = true;
+                        break;
+                    }
+                    n /= 10;
+                }
+            }
+            return aho;
+        }
+    }
+}
