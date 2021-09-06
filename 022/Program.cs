5f7b7ca7432cd51fc88526384d10b3f628e56372
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-07 00:32:28 +0900

    Create Program.cs

---
+++ 022/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eto = new string[]{"亥","子","丑","寅","卯","辰","巳","午","未","申","酉","戌"};
+
+            var n = int.Parse(Console.ReadLine());
+            var j = (n + 9) % 12;
+            Console.WriteLine(eto[j]);
+        }
+    }
+}
