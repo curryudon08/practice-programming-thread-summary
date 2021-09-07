28b823bf3d5af9551091311d88358fd3fef639a3
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-08 03:16:06 +0900

    Create Program.cs

---
+++ 024/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[]{12, 6, 8, 3, 10, 1, 0, 9};
+            var s =  arr
+                .Select((v,i) => new {value = v, index = i})
+                .OrderByDescending(x => x.value)
+                .Take(3);
+
+            foreach(var x in s){
+                Console.WriteLine("{0} => {1}", x.index, x.value);
+            }
+        }
+    }
+}
