f05a1ec7a8648d644bcbf6240e0bb96638dcd84d
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-08 03:35:27 +0900

    Create Program.cs

---
+++ 028/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _028
+{
+    class Program
+    {
+        static string Hiragna = 
+            "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん";
+
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var a = Hiragna[r.Next(Hiragna.Length)];
+            var b = Hiragna[r.Next(Hiragna.Length)];
+            Console.WriteLine("{0} {1}",a,b);
+        }
+    }
+}
