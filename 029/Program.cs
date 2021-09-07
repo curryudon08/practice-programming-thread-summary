43c5fba09a9611dc3a649a57c606f1538a199dc0
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-08 03:57:46 +0900

    Create Program.cs

---
+++ 029/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _029
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var StartYear = 1896;
+            var notHold = new int[]{1916,1940,1944};
+
+            for (int i = 0; StartYear + i <= DateTime.Today.Year; i += 4)
+            {
+                var y = StartYear + i;
+                if(!notHold.Contains(y)){
+                    Console.WriteLine("第{0,2}回 {1}",i / 4 + 1,y);
+                }                
+            }
+        }
+    }
+}
