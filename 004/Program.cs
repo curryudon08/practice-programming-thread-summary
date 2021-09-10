ef03dfbdf28881bd49730e4f6398203ef1984735
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-11 06:00:37 +0900

    Create Program.cs

---
+++ 004/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+
+            //微分を用いて近似していく
+            double b = a;
+            for(int i = 0; i < 1000; i++){
+                b = (b * b + a) / (2 * b);
+            }
+            Console.WriteLine(b);
+        }
+    }
+}
