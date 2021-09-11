8e1beb88c601eaf1a6833dee29cd247f00866d82
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-11 23:41:24 +0900

    Create Program.cs

---
+++ 031/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = Enumerable.Range(1, 100).Select(x => x * x).ToArray();
+
+            for(var n = 1; n <= 10000; n++){
+                var t = n;
+                if(p.Contains(t)){
+                    Console.WriteLine("√{0} -> {1}",n, Math.Sqrt(t));
+                }else{
+                    var x = 1;
+                    foreach(var v in p){
+                        if(t % v == 0){
+                            t /= v;
+                            x *= (int)Math.Sqrt(v);
+                        }
+                    }
+                    /*
+                    if(n > 20){
+                        break;
+                    }*/
+                    if(x > 1){
+                        Console.WriteLine("√{0} -> {1}√{2}",n, x, t);
+                    }else{
+                        Console.WriteLine("√{0} -> √{1}",n, t);
+                    }
+                }
+            }
+        }
+    }
+}
