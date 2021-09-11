d46c9dd8873a2880d8984fcebb2a968b992f913a
udonk <6565941+curryudon08@users.noreply.github.com>
2021-09-11 15:02:45 +0900

    Create Program.cs

---
+++ 007/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _007
+{
+    class Program
+    {
+        static int count = 0;
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Solver(n,"A","B","C");
+        }
+
+        static void Solver(int n, string from, string to, string work)
+        {
+            if(n > 1){
+                Solver(n-1, from, work, to);
+                count++;
+                Console.WriteLine("{0}: {1} to {2}",count,from,to);
+                Solver(n-1, work, to, from);
+            }else{
+                count++;
+                Console.WriteLine("{0}: {1} to {2}",count,from,to);
+            }
+        }
+    }
+}
